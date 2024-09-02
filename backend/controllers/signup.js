16dacd6079c60810da433190c000cbabcbc5cef7
Mr-mahato <cmhandan@gmail.com>
2024-09-03 02:20:57 +0530

    made few changes

--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,57 +1,55 @@
-const {User,Company}=require("../models/userData");
+const { User, Company } = require("../models/userData");
 
-async function findEmpIdOnDatabase(empId_temp){
-    try{
-        let obj=await Company.find({empId:empId_temp});        
-        if(obj.length>0){
-            return ({ status:400,msg:"Emp exists"});
-        }
-        else{
-           return ({status:404,msg:"Emp not found"});
-        }
+async function findEmpIdOnDatabase(empId_temp) {
+  try {
+    let obj = await Company.find({ empId: empId_temp });
+    if (obj.length > 0) {
+      return { status: 400, msg: "Emp exists" };
+    } else {
+      return { status: 404, msg: "Emp not found" };
     }
-    catch(err){
-        return ({status:500,msg:"Error on fetching ID"});
-    }
+  } catch (err) {
+    return { status: 500, msg: "Error on fetching ID" };
+  }
 }
 
+async function SignUp(req, res) {
+  try {
+    console.log(req.body)
+    let obj = await User.find({ empId: req.body.empId });
 
-async function SignUp(req,res){
-    try{
-    let obj=await User.find({empId:req.body.empId});
-    
-    if(obj.length>0){
-        return res.status(400).json({msg:"Employee exists"});
-    }
-    else{
-      let msg1= await findEmpIdOnDatabase(req.body.empId);
-      if(msg1.status===400){
-        // console.log(req.body);        
-       await User.create(req.body);                
-        res.status(200).json({msg:"User created successfully"});
+    if (obj.length > 0) {
+      return res.status(400).json({ msg: "Employee exists" });
+    } else {
+      let msg1 = await findEmpIdOnDatabase(req.body.empId);
+      if (msg1.status === 400) {
+        // console.log(req.body);
+        await User.create(req.body);
+        res.status(200).json({ msg: "User created successfully" });
+      } else if (msg1.status === 404) {
+        res.status(403).json({ msg: "Not allowed" });
+      } else {
+        res.json({ msg: msg1.msg });
       }
-      else if(msg1.status===404){
-        res.status(403).json({msg:"Not allowed"});
-      }
-      else{
-        res.json({msg:msg1.msg});
-      }
-    }    
-}
-catch(err){
-    res.status(500).json({msg:"Registration Failed"});
-}
-
-}
-async function Login(req,res){
-    let obj=await User.find({username:req.body.username,password:req.body.password});
-    if(obj.length>0){
-        res.status(200).json({msg:"Successfully Logged In"});
     }
-    else{
-        res.status(404).json({msg:"User not found"});
+  } catch (err) {
+    res.status(500).json({ msg: "Registration Failed" });
+  }
+}
+async function Login(req, res) {
+  try {
+    let obj = await User.findOne({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    if (obj) {
+      res.status(200).json({ status: true, user:obj ,  msg: "Successfully Logged In" });
+    } else {
+      res.status(404).json({status:false ,  msg: "User not found" });
     }
-
+  } catch (error) {
+    res.status(500).json({status:false , msg:error})
+  }
 }
 
-module.exports={SignUp,Login};
+module.exports = { SignUp, Login };
--- backend/models/userData.js
+++ backend/models/userData.js
@@ -1,26 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String},
-  password:{type:String},
-  dateOfBirth: { type: Date},
+  username: { type: String },
+  password: { type: String },
+  dateOfBirth: { type: Date },
   email: { type: String, unique: true },
-   phone: { type: String },
+  phone: { type: String },
   address: { type: String },
-  empId: { type: String,unique: true },
-   department: { type: String},
-   dateOfJoining: { type: Date},
-  walletAddress: { type: String}
+  empId: { type: String, unique: true },
+  department: { type: String },
+  dateOfJoining: { type: Date },
+  walletAddress: { type: String , default:"null" },
 });
 
-const companySchema=new mongoose.Schema({
-    empId:{type:String,unique:true},
-    fullname:String
-})
+const companySchema = new mongoose.Schema({
+  empId: { type: String, unique: true },
+  fullname: String,
+});
 
-const User = mongoose.model('User', userSchema);
-const Company=mongoose.model("employee",companySchema);
+const User = mongoose.model("User", userSchema);
+const Company = mongoose.model("employee", companySchema);
 
-module.exports ={ User ,
-    Company
-};
+module.exports = { User, Company };
